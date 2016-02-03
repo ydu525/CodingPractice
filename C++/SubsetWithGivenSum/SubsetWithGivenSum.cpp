12173416ef80b310d27d09da10691db98a6227b6
ydu525 <ydu525@gmail.com>
2016-02-02 17:23:38 -0800

    Update SubsetWithGivenSum
    fix a minor bug and clean up comments

--- C++/SubsetWithGivenSum/SubsetWithGivenSum.cpp
+++ C++/SubsetWithGivenSum/SubsetWithGivenSum.cpp
@@ -3,19 +3,29 @@
 
 using namespace std;
 
-/* This puzzle is basically to find a subset inside an array with sum equal to given sum.
-* If we just want to know whether there is subset, we can solve this problem easily with Dynamic Programming.
-* However, we want the specific answer here, so what I am doing is basically listing all the possible subsets.
-* It is an O(2 ^ n) solution. With our n = 26, it is extremely time consuming, and it is 'unsolvable' by my computer.
-* So in my recursive findSubsetHelper function, we have to do branch pruning to cut off unnecessary calculations.
-* I check whether my current sum is larger than the target, if it is, I would just return. However, with my simple pruning logic,
-* it is still ‘unsolvable’ by my computer. I could not come up a better pruning method.
-* Then I calculate the sum of the given numbers and find out that it is close to 100,000,000.
-* So instead of find subset with sum equal to 100,000,000, I would find a subset with sum equal to 29,161,818.
-* Subtracting this subset from the original array, we will get the answer we want.
-* The given sample has a lot similar and small data(for ex. 2149217 and 2134508), it makes my pruning logic very ineffective.
-* Changing the target number to a smaller number makes the computer doing less calculations before making the pruning.
-* As a result, even with the same size input, my personal PC can finish running my code within few seconds.
+/*
+* Thoughts on solving the puzzle:
+*
+* This puzzle is equivalent to find a subset of an array with sum equal to given sum.
+* If we just want to know whether there is such a subset, we can solve this problem easily with Dynamic Programming.
+* However, we want the specific answer here, so I think the best-known algorithm is calculating all possible subsets.
+* It is an O(2 ^ n) solution. With our input n = 26, it is extremely time-consuming, and my computer could not finish the code execution within a considerable time.
+* So inside the recursive findSubsetHelper function, we have to do branch pruning to cut off unnecessary calculations.
+* I check whether my accumulated sum is larger than the target, if it is, I would stop further calculation.
+* However, this simple pruning logic does not improve the run time significantly.
+* Then I did some observation and found out that the sum of the given numbers is close to 100,000,000.
+* So instead of to find subset with sum equal to 100,000,000, I would try to find the complementary subset.
+* Subtracting the complementary subset from the original array, we will get the answer we want.
+* The given sample has many similar and "small" data(compared to our target sum, for ex. 2149217 and 2134508).
+* Subtracting the largest two elements from the array, rest of the array (24 nums) would only sum up to 97,435,872 which is less than 100,000,000.
+* So the pruning condition would not be triggered during most of the calculations.
+* Changing the target sum to a smaller number by finding the complementary subset makes the pruning logic become more effective.
+* As a result, even with the same size input, my personal PC could finish running the code within few seconds.
+*
+* The answer is
+* 	18897109, 12828837, 9461105, 6371773, 5946800, 5582170,
+* 	5268860, 4552402, 4335391, 4296250, 4224851, 3279833,
+* 	3095313, 2812896, 2543482, 2226009, 2142508, 2134411
 */
 
 bool findSubsetHelper(size_t index, vector<unsigned long> &res, unsigned long curSum, vector<unsigned long> &nums, unsigned long expectedSum) {
@@ -61,18 +71,15 @@ int main() {
 
 	//just some obervation codes here
 	unsigned long sum = 0;
-	for (int i = 0; i < 26; ++i) {
+	for (size_t i = 0; i < areas.size(); ++i) {
 		sum += areas[i];
 	}
 	unsigned long overValue = sum - expectedSum;
 	printf("sum is %d\n", sum);
-	printf("average is %d\n", sum / 26);
+	printf("average is %d\n", sum / (unsigned long)areas.size());
 	printf("overValue is %d\n", overValue);
 
-	//subtarct the largert two elements because their sum is 31,725,946 which is larger than the
-	//over value that is 29,161,818
-	vector<unsigned long> subAreas(areas.begin(), areas.end() - 2);
-	vector<unsigned long> numsToBeSubtracted = findSubset(subAreas, overValue);
+	vector<unsigned long> numsToBeSubtracted = findSubset(areas, overValue);
 	vector<unsigned long> res;
 	for (size_t i = 0, j = 0; i < areas.size(); ++i) {
 		if (j >= numsToBeSubtracted.size() || areas[i] != numsToBeSubtracted[j]) {
