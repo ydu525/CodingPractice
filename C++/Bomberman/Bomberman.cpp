5df7316cb7aeb3178c7b1c5f01fb0a1f3a3b9d12
ydu525 <ydu525@gmail.com>
2016-06-20 23:49:36 -0700

    BombEnemy
    is the as problem as LeetCode No.361.
    Second DP version.

--- C++/Bomberman/Bomberman.cpp
+++ C++/Bomberman/Bomberman.cpp
@@ -54,11 +54,53 @@ public:
 		}
 		return res;
 	}
+
+	int mostKillsV2(const vector<vector<int>>& board) {
+		int res = 0;
+		int n = board.size();
+		if (n == 0) {
+			return res;
+		}
+		int m = board[0].size();
+		if (m == 0) {
+			return res;
+		}
+		vector<vector<int>> ver(n + 2, vector<int>(m + 2, 0));
+		vector<vector<int>> hori(n + 2, vector<int>(m + 2, 0));
+
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				if (board[i - 1][j - 1] == -1) {
+					continue;
+				}
+				ver[i][j] = ver[i - 1][j] + board[i - 1][j - 1];
+				hori[i][j] = hori[i][j - 1] + board[i - 1][j - 1];
+			}
+		}
+
+		for (int i = n; i > 0; --i) {
+			for (int j = m; j > 0; --j) {
+				if (board[i - 1][j - 1] == -1) {
+					continue;
+				}
+				int up = ver[i][j];
+				int left = hori[i][j];
+				ver[i][j] = hori[i][j] = 0;
+				ver[i][j] = ver[i + 1][j] + board[i - 1][j - 1];
+				hori[i][j] = hori[i][j + 1] + board[i - 1][j - 1];
+				if (board[i - 1][j - 1] == 0) {
+					res = max(res, up + left + ver[i][j] + hori[i][j]);
+				}
+			}
+		}
+
+		return res;
+	}
 };
 
 int main() {
 	vector<vector<int>> board1 = { { 0, 0, 1, 1, -1 }, { 1, 1, -1, 0, 0 }, { 0, -1, 0, 0, 1 }, { 1, 1, 1, 1, 1 } };
 	Solution s;
-	s.mostKills(board1);
+	s.mostKillsV2(board1);
 	return 0;
 }
